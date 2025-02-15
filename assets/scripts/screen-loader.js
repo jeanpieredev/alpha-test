dc936b12c6b7ed4426bd8dd84d6c01ede392781f
Jrodriguez1001 <2016200197@untels.edu.pe>
2025-02-15 01:38:47 -0500

    fix loader

--- assets/scripts/screen-loader.js
+++ assets/scripts/screen-loader.js
@@ -1,5 +1,7 @@
+document.body.classList.add("loading");
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     document.getElementById("loading-screen").style.display = "none";
+    document.body.classList.remove("loading");
   }, 1000);
 });
