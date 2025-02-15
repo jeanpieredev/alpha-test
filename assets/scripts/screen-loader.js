44d140878449bc0cac3df658a61901444ceccc11
Jrodriguez1001 <2016200197@untels.edu.pe>
2025-02-14 20:51:06 -0500

    add screen loader script

---
+++ assets/scripts/screen-loader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    // Hide the loading screen
+    document.getElementById("loading-screen").style.display = "none";
+  }, 1000);
+});
