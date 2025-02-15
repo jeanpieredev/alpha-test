87822211f70b6c60ac0650944a7b0a65e8055351
Jrodriguez1001 <2016200197@untels.edu.pe>
2025-02-15 00:39:53 -0500

    remove comments

--- assets/scripts/screen-loader.js
+++ assets/scripts/screen-loader.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    // Hide the loading screen
     document.getElementById("loading-screen").style.display = "none";
   }, 1000);
 });
